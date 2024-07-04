7681e6ee32b968d1da84c70e1106a3f2b8947d4f
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-05 00:44:38 +0100

    6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Task 6
+'''
+from typing import List
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''
+    Returns sum of a mixed list
+    '''
+    return float(sum(mxd_lst))
