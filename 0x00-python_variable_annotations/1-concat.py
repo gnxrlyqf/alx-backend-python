9eade0bb951723d0b671b9478f9634dc14355bf3
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-05 00:34:43 +0100

    1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Task 1
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Returns a concatenation of two strings.
+    '''
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Task 2
+'''
+
+
+from math import floor
+
+
+def floor(a: float) -> int:
+    '''
+    Returns a floored float
+    '''
+    return floor(a)
