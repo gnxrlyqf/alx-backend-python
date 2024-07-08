87236d25fe03c364b71bc0c829985422290325f5
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-09 00:12:38 +0100

    0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,14 +3,16 @@
     Task 0
 '''
 
-from random import random
-from asyncio import sleep
+# from random import random
+# from asyncio import sleep
+import random
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
     Delays the return of a random value for the duration of that value
     """
-    delay = random() * max_delay
-    await sleep(delay)
+    delay = random.random() * max_delay
+    await asyncio.sleep(delay)
     return delay
