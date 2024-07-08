7efac1c161a48711750dbcc3914a71db6aa3194b
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-09 00:35:08 +0100

    4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+	Task 4
+"""
+
+from asyncio import as_completed
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Execute wait_random n times
+    """
+    coros = [task_wait_random(max_delay) for coro in range(n)]
+    return [await coro for coro in as_completed(coros)]
