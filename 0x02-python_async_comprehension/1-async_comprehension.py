eef048a302a63dee14331a2122126e6f41e58a16
gnxrlyqf <gnarlydestructor@outlook.com>
2024-07-10 00:42:12 +0100

    1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,12 +3,11 @@
     Task 1
 """
 from typing import List
-Vector = List[float]
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Vector:
+async def async_comprehension() -> List[float]:
     """
     Generate asynchronous coroutines
     """
